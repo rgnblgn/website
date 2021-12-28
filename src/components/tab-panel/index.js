43d988f8d9788a6b288ce5808e204adcbcbaebed
rgnblgn <43182194+rgnblgn@users.noreply.github.com>
2021-12-28 20:22:19 +0300

    Update index.js

--- src/components/tab-panel/index.js
+++ src/components/tab-panel/index.js
@@ -4,23 +4,37 @@ import style from './index.scss'
 export default function TabPanel(props){
     
     const [selectedIndex,setSelectedIndex] = useState(0)
+    const [selectedMenuIndex,setSelectedMenuIndex] = useState(0)
+
 
     const handleLeftClick = (e)=>{
         setSelectedIndex(e)
     }
 
     const handleTopClick = (e)=>{
-        setSelectedIndex(e)
+        setSelectedMenuIndex(e)
     }
 
     const exArr = [0,1,2,3,4,5,6]
+
+    const topMenus = ['Men','Women','Shoes','Tech','Kids','Car','Home']
+
+    const menSubmenus = ['Shirt','Jacket','Shorts','Jeans','Suits','Underwear','Coats']
+    const womenSubmenus = ['Dresses','Skirts','Shorts','Jeans','Blouse','Underwear','Coats']
+    const shoesSubmenus = ['Men','Women','Kids','Sport','Classic','Winter','Summer']
+    const techSubmenus = ['PC','Mobile','Television','Game','White','Drones','Accessory']
+    const kidsSubmenus = ['Shirts','Shorts','Shoes','Toys','Baby','Furniture','Etc.']
+    const carsSubmenus = ['Cars1','Cars2','Cars3','Cars4','Cars5','Cars6','Cars7']
+    const homeSubmenus = ['Home1','Home2','Home3','Home4','Home5','Home6','Home7']
+
+    const subMenuArr = [menSubmenus,womenSubmenus,shoesSubmenus,techSubmenus,kidsSubmenus,carsSubmenus,homeSubmenus]
 
     return <div className='tabPanelContainer'>
         <div className='leftPanel'>
             <ul>
                 {
-                exArr.map((item,index)=>{
-                    return <li key={index} onClick={()=>handleLeftClick(index)}>Sol Seçenek</li>
+                subMenuArr[selectedMenuIndex].map((item,index)=>{
+                    return <li key={index} onClick={()=>handleLeftClick(index)}>{item}</li>
                 })
                 }
             </ul>
@@ -28,8 +42,8 @@ export default function TabPanel(props){
         <div className='topPanel'>
              <ul>
              {
-                exArr.map((item,index)=>{
-                    return <li key={index} onClick={()=>handleTopClick(index)}>Üst Seçenek</li>
+                topMenus.map((item,index)=>{
+                    return <li key={index} onClick={()=>handleTopClick(index)}>{item}</li>
                 })
             } 
             </ul>
