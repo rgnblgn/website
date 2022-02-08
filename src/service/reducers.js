584a48c5366f2a2d77f532a2f838cc24df6b1b6b
rgnblgn <43182194+rgnblgn@users.noreply.github.com>
2022-02-08 20:37:48 +0300

    Add files via upload

--- src/service/reducers.js
+++ src/service/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import imagesReducer from './images/reducers';
+import tabsReducer from './tab-panel/reducer'
 
 export default combineReducers({
-  images: imagesReducer
+  images: imagesReducer,
+  tabs:tabsReducer
 });
